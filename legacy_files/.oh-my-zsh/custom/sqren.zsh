255a9c36841f29ae78a576ef42b111e7299c7350
Søren Louv-Jansen <sorenlouv@gmail.com>
2014-11-30 21:26:51 -0800

    Added show/hide hidden files

--- legacy_files/.oh-my-zsh/custom/sqren.zsh
+++ legacy_files/.oh-my-zsh/custom/sqren.zsh
@@ -8,4 +8,8 @@ export EDITOR=sublime
 # Grunt tab completion
 eval "$(grunt --completion=zsh)"
 
+# Show hidden files
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 
+# Hide hidden files
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
